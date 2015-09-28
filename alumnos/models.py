4b905b0b7ea41717297d4ec74339c28203275db1
Jon Latorre <jonlatorremartinez@gmail.com>
2015-09-28 21:01:04 +0200

    Modelos basicos

--- alumnos/models.py
+++ alumnos/models.py
@@ -1,6 +1,7 @@
+# -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
-
+from profesores.models import Profesor
 
 class Alumno(models.Model):
     nombre = models.CharField(max_length=25,default="")
@@ -18,4 +19,83 @@ class Alumno(models.Model):
     #    return "%s %s (%s)"%(self.user.get_short_name(),self.user.last_name,self.user.username)
 
     def get_absolute_url(self):
-        return "/alumno/%s/"%self.id
+        return "/alumnos/%s/"%self.id
+
+class Ano(models.Model):
+	nombre = models.CharField(max_length=25,default="")
+	activo = models.BooleanField(default=False)
+
+class Asistencia(models.Model):
+    ano = models.ForeignKey('Ano')
+    grupo = models.ForeignKey('Grupo')
+    alumno = models.ForeignKey('Alumno')
+    confirmado = models.BooleanField(default=0)
+    factura = models.BooleanField(default=0)
+    metalico = models.BooleanField(default=0)
+    precio = models.BooleanField(default="")
+    #~ notas = MultipleJoin('Notas')
+    #~ faltas = MultipleJoin('Notas')
+
+class Historia(models.Model):
+	alumno = models.ForeignKey('Alumno')
+	fecha = models.DateField(auto_now_add=True)
+	tipo = models.CharField(max_length=25,default="")
+	anotacion = models.CharField(max_length=25,default="")
+
+class Aula(models.Model):
+    ##nombre = models.CharField()
+    numero = models.DecimalField(max_digits=3,decimal_places=0)
+    piso = models.CharField(max_length=25,default="")
+    aforo = models.DecimalField(max_digits=3,decimal_places=0,default=14)
+
+#~ class Clase(models.Model):
+    #~ dia_semana = models.CharField(max_length=15,default="")
+    #~ aula = models.ForeignKey('Aula')
+    #~ horario = models.CharField(max_length=25,default="")
+    #~ profesor = models.ManyToManyField('Profesor')
+    #~ grupo = models.ManyToManyField('Grupo')
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=25,default="")
+    precio = models.FloatField(default=100)
+    ##No obligatorios
+    examen = models.CharField(max_length=25,default="")
+    nivel = models.CharField(max_length=25,default="")
+    libros = models.ManyToManyField('Libro')
+    nota_aprobado = models.FloatField(default=50)
+    solo_examen_final = models.BooleanField(default=False)
+
+class Grupo(models.Model):
+    nombre = models.CharField(max_length=25,default="")
+    curso = models.ForeignKey('Curso')    
+    num_max = models.DecimalField(max_digits=2,decimal_places=0,default=14) #El tamano default no es lo mejor que este aqui, pero bueno
+    menores = models.BooleanField(default=False)
+
+class Festivo(models.Model):
+    ano = models.DecimalField(max_digits=4,decimal_places=0)
+    mes = models.DecimalField(max_digits=2,decimal_places=0)
+    dia = models.DecimalField(max_digits=2,decimal_places=0)
+    inicio = models.BooleanField(default=False)
+    fin = models.BooleanField(default=False)
+    observaciones = models.CharField(max_length=250,default="",blank=True,null=True)
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=25,default="")
+    autor = models.CharField(max_length=25,default="")
+    isbn = models.CharField(max_length=25,default="")
+    editorial = models.CharField(max_length=25,default="")
+class Nota(models.Model):
+    asistencia = models.ForeignKey('Asistencia')
+    trimestre = models.DecimalField(max_digits=1,decimal_places=0)
+    grama = models.DecimalField(max_digits=3,decimal_places=0,default=0)
+    grama_baremo = models.DecimalField(max_digits=3,decimal_places=0,default=0)
+    expresion = models.DecimalField(max_digits=3,decimal_places=0,default=0)
+    expresion_baremo = models.DecimalField(max_digits=3,decimal_places=0,default=0)
+    lectura = models.DecimalField(max_digits=3,decimal_places=0,default=0)
+    lectura_baremo = models.DecimalField(max_digits=3,decimal_places=0,default=0)
+    
+class Falta(models.Model):
+    asistencia = models.ForeignKey('Asistencia')
+    mes = models.DecimalField(max_digits=1,decimal_places=0)
+    justificadas = models.DecimalField(max_digits=3,decimal_places=0,default="0")
+    faltas = models.DecimalField(max_digits=3,decimal_places=0,default="0")
--- kontulari/management/commands/importacion.py
+++ kontulari/management/commands/importacion.py
@@ -4,6 +4,8 @@
 __author__ = 'patataman'
 from django.core.management.base import BaseCommand, CommandError
 from alumnos.models import Alumno as Alumno_new
+from alumnos.models import Libro as Libro_new
+from alumnos.models import Curso as Curso_new
 
 from sqlobject import *
 from sqlobject.inheritance import InheritableSQLObject
@@ -18,21 +20,45 @@ class Command(BaseCommand):
         else:
             bbdd = args[0]
             self.stdout.write('Iniciando importacion desde archivo %s'%bbdd)
-            self.stdout.write('Vamos a vaciar la BBDD')
-            Alumno_new.objects.all().delete()
             sqlhub.processConnection = connectionForURI('sqlite://'+bbdd)
             from alumnos.old_database_model import *
+
+            self.stdout.write('Importamos los libros, primero vamos a vaciar la BBDD')
+            Libro_new.objects.all().delete()
+            busqueda = Libro.select()
+            self.stdout.write('Encontrados %d libros'%busqueda.count())
+            for libro in busqueda:
+                l = Libro_new(\
+                    titulo=libro.titulo,\
+                    autor=libro.autor,\
+                    isbn=libro.isbn,\
+                    editorial=libro.editorial,\
+                )
+                l.save()
+            
+            self.stdout.write('Importamos los cursos, primero vamos a vaciar la BBDD')
+            Curso_new.objects.all().delete()
+            busqueda = Curso.select()
+            self.stdout.write('Encontrados %d cursos'%busqueda.count())
+            #~ for curso in busqueda:
+                #~ c = Curso_new(\
+                    #~ 
+                #~ )
+                #~ c.save()
+            #~ 
+            self.stdout.write('Importamos los alumnos, primero vamos a vaciar la BBDD')
+            Alumno_new.objects.all().delete()
             busqueda = Alumno.select()
             self.stdout.write('Encontrados %d alumnos'%busqueda.count())
             for persona in busqueda:
                 #self.stdout.write('Anadiendo el alumno %d'%persona.id)
                 try:
-                    cuenta = "%d%d%d%d"%(persona.banco.codigo,persona.sucursal,persona.dc,persona.cuenta)
+                    cuenta = "%d-%d-%d-%d"%(persona.banco.codigo,persona.sucursal,persona.dc,persona.cuenta)
                 except:
                     cuenta = ""
                     self.stdout.write('Problema con la cuenta bancaria del alumno %d'%persona.id)
                 a = Alumno_new(id=persona.id,\
-					activo = persona.activo,\
+                    activo = persona.activo,\
                     nombre = persona.nombre,\
                     apellido1 = persona.apellido1,\
                     apellido2 = persona.apellido2,\
